79821487fd1f65bed8810558a40f8768b9dcb49a
호병준 <qudwns4892@naver.com>
2021-10-03 17:47:45 +0900

    Initial Commit

---
+++ number.py
@@ -0,0 +1,19 @@
+my_name='Python' #문자열 (사람을 위한 텍스트를 프로그래머가 부르는 방법)
+my_age= 2016-1991
+print(my_name,'은 이제', my_age,'살')
+
+my_next_age = my_age + 1
+print(my_name,'은 이제', my_next_age,'살')
+
+multiply=9*9 
+divide=30/5
+power=2**10
+reminder=15%4
+
+print(multiply,divide,power,reminder)
+
+text='2015'+'1991'
+number=2015+1991
+print(text)
+print(number)
+
---
+++ variable.py
@@ -0,0 +1,6 @@
+identity = '지구인'
+number_of_legs=4
+print('안녕! 나는', identity,'이야. 나는 다리가',number_of_legs,'개 있어.')
+identity = '외계인'
+number_of_legs=21
+print('안녕! 나는', identity,'이야. 나는 다리가',number_of_legs,'개 있어.')
